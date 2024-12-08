610522fd4fa16e5f108321bc13fd41b6983281d5
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-12-08 23:21:30 +0530

    done

--- app/dashboard/settings/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div> Settings page</div>
-  )
-}
-
-export default page
--- app/greeting/[name]/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = ({ params }) => {
-  return (
-    <div>Nice to meet you. {params.name}</div>
-  )
-}
-
-export default page
